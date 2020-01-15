816cc5fa23e6c50cca24c81ee1854537155e8a2b
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-01-15 18:40:50 -0300

    Add mongo connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:KGMHclJRzTGCenoG@cluster0-lr1jq.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
@@ -10,6 +16,8 @@ app.use(express.json());
 // Query Params: request.query (filtros, ordenação, paginação, ...)
 // Route Params: reuest.params (identificar um recurso na alteração ou remoção)
 // Body: request.body (dDados para criação ou alteração do registro)
+
+// MongoDB (banco não relacional)
 
 app.get('/', (request, response) => {
     return response.json({ message: "Página inicial" });
