00c6ba6b890dca431087af94ce105d79028ea159
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-01-15 17:57:57 -0300

    Create test routes

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const app = express();
 
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de Parâmetros
+// Query Params: request.query (filtros, ordenação, paginação, ...)
+// Route Params: reuest.params (identificar um recurso na alteração ou remoção)
+// Body:
+
 app.get('/', (request, response) => {
-    return response.json({ message: "Agora vai" });
+    return response.json({ message: "Página inicial" });
+});
+
+app.get('/users', (request, response) => {
+    console.log(request.query);
+    return response.json({ message: "Buscando usuários" });
+});
+
+app.delete('/users/:id', (request, response) => {
+    console.log(request.params);
+    return response.json({ message: "Deletando usuários" });
 });
 
 app.listen(3333)
