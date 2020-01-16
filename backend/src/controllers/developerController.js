5de8e857870a58dea3dc352ef3057bc4080401a9
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-01-15 23:51:27 -0300

    Add condition to prevent duuplicate dev register

--- backend/src/controllers/developerController.js
+++ backend/src/controllers/developerController.js
@@ -7,26 +7,29 @@ module.exports = {
     
         const { github_username, techs, latitude, longitude } = request.body;
     
-        const apiResponse = await axios.get(`http://api.github.com/users/${github_username}`);
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
-    
-        const { name = login, avatar_url, bio } = apiResponse.data;
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
-        };
-    
-        const dev = await developer.create({
-            name,
-            avatar_url,
-            bio,
-            github_username,
-            techs: techsArray,
-            location
-        });
-    
+        let dev = await developer.findOne({github_username});
+
+        if (!dev) {
+            const apiResponse = await axios.get(`http://api.github.com/users/${github_username}`);
+        
+            const techsArray = techs.split(',').map(tech => tech.trim());
+        
+            const { name = login, avatar_url, bio } = apiResponse.data;
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            };
+        
+            dev = await developer.create({
+                name,
+                avatar_url,
+                bio,
+                github_username,
+                techs: techsArray,
+                location
+            });
+        }
         return response.json(dev);
     }
 };
