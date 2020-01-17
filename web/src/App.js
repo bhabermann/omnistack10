e805189a4e013b2ca9f1dd41af484d64133b549e
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-01-16 23:08:40 -0300

    Create sidebar

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './global.css';
+import './App.css'
+import './Sidebar.css'
 
 // Principais conceitos do React
 // Componente: Bloco isolado de html, css e js que não interfere no restante da aplicação.
@@ -13,7 +15,32 @@ function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
 
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
       <main>
 
