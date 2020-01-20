cc1d4320ad5ad481de4f03b8c3e622b564ee687e
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-01-19 22:21:28 -0300

    Create search input

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState();
@@ -34,20 +35,35 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -22.8963676, longitude: -47.0469528}}>
-                <Image style={styles.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/7130275?s=460&v=4'}} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', {github_username: 'bhabermann'});
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Bruno Habermann</Text>
-                        <Text style={styles.devBio}>Uma bio aleatória só pra encher linguiça nesta parte da aula e ver como fica o resultado</Text>
-                        <Text style={styles.devTechs}>.Net, Delphi, React, SQL Server</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -22.8963676, longitude: -47.0469528}}>
+                    <Image style={styles.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/7130275?s=460&v=4'}} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', {github_username: 'bhabermann'});
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Bruno Habermann</Text>
+                            <Text style={styles.devBio}>Uma bio aleatória só pra encher linguiça nesta parte da aula e ver como fica o resultado</Text>
+                            <Text style={styles.devTechs}>.Net, Delphi, React, SQL Server</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por Techs"
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#ffffff" />
+                </TouchableOpacity>
+            </View>
+        </>
     )  
 }
 
@@ -80,6 +96,44 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        // iOS
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        // Android
+        elevation: 3,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
 })
 
