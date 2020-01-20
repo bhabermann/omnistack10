601fc476c2565d985a6156fec532881547348f52
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-01-19 22:06:41 -0300

    Create Profile Page

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -3,7 +3,7 @@ import { StyleSheet, Image, View, Text } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-function Main() {
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState();
 
     useEffect(() => {
@@ -37,7 +37,9 @@ function Main() {
         <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -22.8963676, longitude: -47.0469528}}>
                 <Image style={styles.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/7130275?s=460&v=4'}} />
-                <Callout>
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', {github_username: 'bhabermann'});
+                }}>
                     <View style={styles.callout}>
                         <Text style={styles.devName}>Bruno Habermann</Text>
                         <Text style={styles.devBio}>Uma bio aleatória só pra encher linguiça nesta parte da aula e ver como fica o resultado</Text>
@@ -46,7 +48,7 @@ function Main() {
                 </Callout>
             </Marker>
         </MapView>
-    )
+    )  
 }
 
 const styles = StyleSheet.create({
